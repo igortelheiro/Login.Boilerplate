f0f11d4b1f96379c59cce0f47c84d79651208f23
Igor Telheiro <igor.telheiro@hotmail.com>
2021-12-08 12:02:43 -0300

    EventBusAdaptar dependencies update

--- Login.EventBusAdapter/EventBusConfig.cs
+++ Login.EventBusAdapter/EventBusConfig.cs
@@ -1,16 +1,12 @@
 ﻿using IntegrationEventLogEF.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using RabbitMQEventBus.Configuration;
 
 namespace Login.EventBusAdapter
 {
     public static class EventBusConfig
     {
-        public static void ConfigureEventBus(this IServiceCollection services, IConfiguration configuration)
-        {
+        public static void ConfigureEventBus(this IServiceCollection services, IConfiguration configuration) =>
             services.ConfigureIntegrationEventLog(configuration);
-            services.ConfigureRabbitMQEventBus();
-        }
     }
 }
